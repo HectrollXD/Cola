38b8439a2617b12366cf382eb3a1601b3cd5c366
Hector Rodriguez <hector.rmtnez89@gmail.com>
2024-09-24 20:54:19 -0600

    Se agrega la suma de 2 variables tipo entero

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 
 int main() {
-	cout<<"Hola mundo"<<endl;
+	int numero1 = 10;
+	int numero2 = 20;
+
+	cout<<"el resultado de la suma de numero 1 y numero 2 = "<<numero1 + numero2<<endl;
 
 	return 0;
 }
