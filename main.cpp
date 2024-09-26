5c18974c19213f7906f1a3d4b4c64d70bd70ff32
Hector Rodriguez <hector.rmtnez89@gmail.com>
2024-09-25 20:30:29 -0600

    se cambia la suma por la resta

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@ int main() {
 	int numero1 = 10;
 	int numero2 = 20;
 
-	cout<<"el resultado de la suma de numero 1 y numero 2 = "<<numero1 + numero2<<endl;
+	cout<<"el resultado de la resta de numero 1 y numero 2 = "<<numero1 - numero2<<endl;
 	
-
 	return 0;
 }
